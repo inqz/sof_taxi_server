e7ab45567453d36074320e96b0c18f23f0cfbacc
darkfree97 <darkfree97@gmail.com>
2018-08-27 12:43:49 +0300

    add heroku sets

--- taxi_app/urls.py
+++ taxi_app/urls.py
@@ -4,13 +4,21 @@ from . import views
 app_name = "taxi_app"
 
 urlpatterns = [
-    path("route/", views.latlng),
-    path("client/", views.client_index),
     path("client/registration/", views.client_registration),
     path("client/authorization/", views.client_authorization),
     path("client/order/create/", views.client_create_new_order),
     path("client/order/cancel/", views.client_cancels_order),
     path("client/request/destinations/", views.client_requests_favourite_destinations),
     path("client/request/driver/position/", views.client_requests_driver_position),
-    path("driver/request/orders/", views.driver_requests_orders)
+    path("client/driver/like/", views.client_send_like_to_driver),
+    path("client/driver/response/", views.client_send_new_response_about_driver),
+    path("driver/registration/", views.driver_registration),
+    path("driver/authorization/", views.driver_authorization),
+    path("driver/status/change/", views.driver_change_status),
+    path("driver/request/orders/", views.driver_requests_orders),
+    path("driver/order/accept/", views.driver_accept_order),
+    path("driver/order/arrived/", views.driver_arrived_to_client),
+    path("driver/order/ride/", views.driver_start_ride),
+    path("driver/order/finish/", views.driver_finish_order),
+    path("driver/order/cancel/", views.driver_cancel_order),
 ]
